ef7571370aadf1b6dca601bca32aff02fa8db56d
harshit-dugar <dugar87711@gmail.com>
2023-02-03 19:36:28 +0530

    changes

--- src/container/Testinomial.jsx
+++ src/container/Testinomial.jsx
@@ -6,7 +6,13 @@ function Testinomail(){
     return(
         <div className="container">
             <p className="test_p">What they say</p>
-            <Carousel>
+            <Carousel
+             showArrows={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={6100}>
                 <Carousel.Item>
                     <div className="testinomial">
                         <p>“Cras habitant et a amet id. Fringilla consequat tortor sit rhoncus mi pellentesque rhoncus. Dictum te\r eu. Ultricies eu aenean pellentesque malesuada. Sem pharetra aliquam tristique aenean purus. Parturient pellentesque habitant proin pellentesque. Eu semper ornare cursus volutpat id. Tellus.”</p>
