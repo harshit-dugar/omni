23f5cc3b1a5cbfa8840b67159c43f3b677433395
harshit-dugar <dugar87711@gmail.com>
2023-02-11 11:34:28 +0530

    testimonial

--- src/components/business/Benefit_B.jsx
+++ src/components/business/Benefit_B.jsx
@@ -4,9 +4,24 @@ import benefit2 from '../../assets/business/Illus2.svg';
 import benefit3 from '../../assets/business/Illus 3.svg';
 
 function Benefit_B(){
+    function reveal(){
+        var reveals = document.getElementsByClassName("reveal");
+        for(var i = 0; i < reveals.length; i++){
+            var windowheight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var revealPoint = 300;
+            if(elementTop < windowheight - revealPoint){
+                reveals[i].classList.add('active');
+            }else{
+                reveals[i].classList.remove('active');
+            }
+        }
+    }
+    window.addEventListener('scroll',reveal);
     return(
+        <div>
         <div className="business_container">
-            <div className="business_section">
+            <div className="business_section reveal">
                 <div className="business_section_left">
                     <h2>Passive Income</h2>
                     <p>Consequat ac sed mi cras at consectetur risus feugiat pretium. Fermentum elit suspendisse eu tortor lectus. Turpis tristique porttitor morbi tellus ac odio fermentum. Amet odio nullam arcu velit ac congue gravida. Congue rutrum felis odio augue neque nibh fringilla. Imperdiet volutpat aenean felis quam condimentum condimentum diam. </p>
@@ -16,7 +31,9 @@ function Benefit_B(){
                     <img src={benefit1} alt="benfits" />
                 </div>
             </div>
-            <div className="business_section">
+        </div>
+        <div className="business_container">
+            <div className="business_section reveal">
                 <div className="business_section_left">
                     <h2>Passive Income</h2>
                     <p>Consequat ac sed mi cras at consectetur risus feugiat pretium. Fermentum elit suspendisse eu tortor lectus. Turpis tristique porttitor morbi tellus ac odio fermentum. Amet odio nullam arcu velit ac congue gravida. Congue rutrum felis odio augue neque nibh fringilla. Imperdiet volutpat aenean felis quam condimentum condimentum diam. </p>
@@ -26,7 +43,9 @@ function Benefit_B(){
                     <img src={benefit2} alt="benfits" />
                 </div>
             </div>
-            <div className="business_section">
+        </div>
+        <div className="business_container">
+            <div className="business_section reveal">
                 <div className="business_section_left">
                     <h2>Passive Income</h2>
                     <p>Consequat ac sed mi cras at consectetur risus feugiat pretium. Fermentum elit suspendisse eu tortor lectus. Turpis tristique porttitor morbi tellus ac odio fermentum. Amet odio nullam arcu velit ac congue gravida. Congue rutrum felis odio augue neque nibh fringilla. Imperdiet volutpat aenean felis quam condimentum condimentum diam. </p>
@@ -36,6 +55,7 @@ function Benefit_B(){
                     <img src={benefit3} alt="benfits" />
                 </div>
             </div>
+        </div>
         </div>
     );
 }
